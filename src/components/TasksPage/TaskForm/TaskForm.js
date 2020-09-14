cc7ed6079ab03ebd239b58111020b6486c8d0996
Timur Kalimullin <timur_kalimullin@list.ru>
2020-09-15 01:46:34 +0500

    fix: made taskform functional, used useRef

--- src/components/TasksPage/TaskForm/TaskForm.js
+++ src/components/TasksPage/TaskForm/TaskForm.js
@@ -5,84 +5,84 @@ import { layout, validateMessages, config, levels } from '../constants';
 
 import './taskform.css';
 
-const authorName = 'Timur Kalimullin'
-class TaskForm extends React.Component {
-  form = React.createRef();
-  render() {
-    const { data, taskModal, onCancel, onFinish, confirmLoading, removeScopeItem } = this.props;
+const authorName = 'Timur Kalimullin';
 
-    const scopeList = Object.keys(levels).map(key => {
-      return <React.Fragment key={key}>
-        <h2 style={{ color: "gray" }}>{levels[key]}</h2>
-        {data && <FormListEdit category={key} data={data} removeScopeItem={removeScopeItem} />}
-        <FormList name={key} />
-      </React.Fragment>
-    })
+const TaskForm = (props) => {
+  const form = React.useRef();
 
-    return (
-      <React.Fragment>
-        <Modal
-          title={<h2 style={{ fontSize: "3em" }}>Task {data ? 'edit' : 'create'}</h2>}
-          minWidth={"500px"}
-          style={{ textAlign: "center" }}
-          width={"900px"}
-          visible={taskModal}
-          okText="Submit"
-          confirmLoading={confirmLoading}
-          cancelText="Cancel"
-          destroyOnClose={true}
-          onCancel={onCancel}
-          onOk={() => {
-            this.form.current.validateFields()
-              .then((values) => {
-                data ? onFinish(values, 'edit') : onFinish(values, 'create')
-              }).then(() => this.form.current.resetFields())
-              .catch((() => message.error('Something went wrong!')))
-          }}>
-          <Form {...layout} name="nest-messages"
-            ref={this.form}
-            validateMessages={validateMessages}>
+  const { data, taskModal, onCancel, onFinish, confirmLoading, removeScopeItem } = props;
 
-            <Form.Item name={['task', 'taskName']} label="Name" rules={[{ required: true }]}
-              initialValue={data ? data.taskName : ''}>
-              <Input />
-            </Form.Item>
+  const scopeList = Object.keys(levels).map(key => {
+    return <React.Fragment key={key}>
+      <h2 style={{ color: "gray" }}>{levels[key]}</h2>
+      {data && <FormListEdit category={key} data={data} removeScopeItem={removeScopeItem} />}
+      <FormList name={key} />
+    </React.Fragment>
+  })
 
-            <Form.Item name={['task', 'description']} label="Description"
-              initialValue={data ? data.description : ''}>
-              <Input.TextArea />
-            </Form.Item>
+  return (
+    <React.Fragment>
+      <Modal
+        title={<h2 style={{ fontSize: "3em" }}>Task {data ? 'edit' : 'create'}</h2>}
+        minWidth={"500px"}
+        style={{ textAlign: "center" }}
+        width={"900px"}
+        visible={taskModal}
+        okText="Submit"
+        confirmLoading={confirmLoading}
+        cancelText="Cancel"
+        destroyOnClose={true}
+        onCancel={onCancel}
+        onOk={() => {
+          form.current.validateFields()
+            .then((values) => {
+              data ? onFinish(values, 'edit') : onFinish(values, 'create')
+            }).then(() => form.current.resetFields())
+            .catch((() => message.error('Something went wrong!')))
+        }}>
+        <Form {...layout} name="nest-messages"
+          ref={form}
+          validateMessages={validateMessages}>
 
-            <Form.Item name={['task', 'state']} label="State"
-              initialValue={data ? data.state : ''}>
-              <Select>
-                <Select.Option value="DRAFT">Draft</Select.Option>
-                <Select.Option value="COMPLETE">Complete</Select.Option>
-                <Select.Option value="ARCHEIVED">Archeived</Select.Option>
-              </Select>
-            </Form.Item>
+          <Form.Item name={['task', 'taskName']} label="Name" rules={[{ required: true }]}
+            initialValue={data ? data.taskName : ''}>
+            <Input />
+          </Form.Item>
 
-            <Form.Item name={['task', 'startTime']} label="Start" {...config}>
-              <DatePicker />
-            </Form.Item>
+          <Form.Item name={['task', 'description']} label="Description"
+            initialValue={data ? data.description : ''}>
+            <Input.TextArea />
+          </Form.Item>
 
-            <Form.Item name={['task', 'endTime']} label="Deadline" {...config}>
-              <DatePicker />
-            </Form.Item>
+          <Form.Item name={['task', 'state']} label="State"
+            initialValue={data ? data.state : ''}>
+            <Select>
+              <Select.Option value="DRAFT">Draft</Select.Option>
+              <Select.Option value="COMPLETE">Complete</Select.Option>
+              <Select.Option value="ARCHEIVED">Archeived</Select.Option>
+            </Select>
+          </Form.Item>
 
-            <Form.Item name={['task', 'author']} label="Author"
-              initialValue={data ? data.author : authorName} hidden={true}>
-              <Input />
-            </Form.Item>
+          <Form.Item name={['task', 'startTime']} label="Start" {...config}>
+            <DatePicker />
+          </Form.Item>
 
-            <h2 >Scopes</h2>
+          <Form.Item name={['task', 'endTime']} label="Deadline" {...config}>
+            <DatePicker />
+          </Form.Item>
 
-            {scopeList}
-          </Form>
-        </Modal>
-      </React.Fragment>
-    );
-  }
+          <Form.Item name={['task', 'author']} label="Author"
+            initialValue={data ? data.author : authorName} hidden={true}>
+            <Input />
+          </Form.Item>
+
+          <h2 >Scopes</h2>
+
+          {scopeList}
+        </Form>
+      </Modal>
+    </React.Fragment>
+  );
 };
 
 export default TaskForm;
